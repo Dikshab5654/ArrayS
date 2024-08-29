c1632ff180260f1373c237d72489d2a7f81b0072
Diksha bargali <119282050+Dikshab5654@users.noreply.github.com>
2024-08-29 22:35:49 +0530

    Array Leaders

--- gfg.cpp
+++ gfg.cpp
@@ -1,7 +1,7 @@
 class Solution {
     // Function to find the leaders in the array.
   public:
-    vector<int> leaders(int n, int a[]) {
+    vector<int> leaders(int n, int a[]) 
         vector<int>ans;
         int maxi = INT_MIN; 
         for(int i=n-1;i>=0;i--){
